766b47dcaad5a8b106cad6fae67268137c452011
Ashley Vicary <ashley.vicary@outlook.com>
2024-11-28 09:33:42 -0500

    Added content

---
+++ assets.R
@@ -0,0 +1,18 @@
+# ---- mindmap-a ----
+mindmap
+  root((me))
+    Research Topics
+      Public Transportation
+      Education
+    Tools
+      Maths
+      Econometrics
+        Causal<br/>Inference
+    Theory
+      International Development
+        China's Development
+        Capitalism
+      Economics
+        History of Economic Thought
+    Blog
+    About
